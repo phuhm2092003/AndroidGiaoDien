336857d232f2f65ddf59c00b4a7961b3cff9dea8
DELL <hominphuzxc@gmail.com>
2022-09-30 17:53:08 +0700

    Android Giao Diện

--- app/src/main/java/fpt/edu/appmanagement/ui/WelcomeActivity.java
+++ app/src/main/java/fpt/edu/appmanagement/ui/WelcomeActivity.java
@@ -21,6 +21,5 @@ public class WelcomeActivity extends AppCompatActivity {
                 startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
             }
         }, 1000);
-
     }
 }
